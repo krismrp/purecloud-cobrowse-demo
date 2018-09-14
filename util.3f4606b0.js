a4b42259456ab3411ccd95a1e5b6ea8902511a2b
Kristian Pettersen <krismrp@gmail.com>
2018-09-14 10:38:48 +0200

    Updates

--- util.3f4606b0.js
+++ util.3f4606b0.js
@@ -606,7 +606,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52356' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54917' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
